cfa793168da09be7bc51ebaf885425cb56c7c1ea
Gohyunyoung <gohyunyoung98@gmail.com>
2016-05-16 12:42:39 +0900

    2016/05/16 commit

--- BoxScoreParser.py
+++ BoxScoreParser.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 from bs4 import BeautifulSoup
@@ -11,53 +11,59 @@ import urllib2
 import re
 
 
-# In[1]:
+# In[2]:
 
 #URL넣어주면 해당 박스스코어 파싱
 class BoxScoreParser:
     def __init__(self,url):
-        self.data = urllib2.urlopen(url)
-        self.html = BeautifulSoup(self.data)
+        data = urllib2.urlopen(url)
+        html = BeautifulSoup(data)
         
         #DATE
-        self.date = self.html.select('div.yearDate span')[0].text
+        self.date = html.select('div.yearDate span')[0].text
         
         #AWAY팀명
-        self.a_Team=self.html.select('table.socreBoard tr:nth-of-type(2) th')[0].text
+        self.a_team=html.select('table.socreBoard tr:nth-of-type(2) th')[0].text
         
         #HOME팀명
-        self.h_Team=self.html.select('table.socreBoard tr:nth-of-type(3) th')[0].text
+        self.h_team=html.select('table.socreBoard tr:nth-of-type(3) th')[0].text
         
         #AWAY_SCORE
-        tmp=self.html.select('table.socreBoard tr:nth-of-type(2) td')
-        self.a_Score=list()
+        tmp=html.select('table.socreBoard tr:nth-of-type(2) td')
+        self.a_score=list()
         for score in tmp:
-            self.a_Score.append(score.text)
+            if score.text==u'-':
+                self.a_score.append(0)
+            else:
+                self.a_score.append(int(score.text))
         
         #HOME_SCORE
-        tmp=self.html.select('table.socreBoard tr:nth-of-type(3) td')
-        self.h_Score=list()
+        tmp=html.select('table.socreBoard tr:nth-of-type(3) td')
+        self.h_score=list()
         for score in tmp:
-            self.h_Score.append(score.text)
+            if score.text==u'-':
+                self.h_score.append(0)
+            else:
+                self.h_score.append(int(score.text))
         
         #AWAY_STATUS
-        self.a_status=self.html.select('p.results')[0].text
+        self.a_status=html.select('p.results')[0].text
         
         #HOME_STATUS
-        self.h_status=self.html.select('p.results')[1].text
+        self.h_status=html.select('p.results')[1].text
         
         #STADIUM
-        tmp=self.html.select('p.ballpark')[0]
+        tmp=html.select('p.ballpark')[0]
         #.text 메서드는 unicode형태로 반환
         tmp=tmp.text
         self.stadium=tmp[tmp.find(u'구장')+4:tmp.find(u'관중')].strip()
         
         #HEAD
-        headRowNum = len(self.html.select('table.tEx tbody tr'))
+        headRowNum = len(html.select('table.tEx tbody tr'))
         head=dict()
         for i in range(headRowNum):
-            key = self.html.select('table.tEx tr:nth-of-type(%d) th'%(i+1))[0].text
-            dat = self.html.select('table.tEx tr:nth-of-type(%d) td'%(i+1))[0].text
+            key = html.select('table.tEx tr:nth-of-type(%d) th'%(i+1))[0].text
+            dat = html.select('table.tEx tr:nth-of-type(%d) td'%(i+1))[0].text
             dat = dat.strip()
             
             if key==u'심판':
@@ -68,7 +74,7 @@ class BoxScoreParser:
         
         #AWAY 투수기록
         columns=[u'선수명',u'등판',u'결과',u'승',u'패',u'세',u'이닝',u'타자',u'투구수',u'타수',u'피안타',u'홈런',u'4사구',u'삼진',u'실점',u'자책',u'평균자책점']
-        frame=self.html.select('table#xtable3 tbody:nth-of-type(1) tr')
+        frame=html.select('table#xtable3 tbody:nth-of-type(1) tr')
         data=list()    #frame으로부터 데이터를 담을 리스트
         for row in frame:
             tmp=list()
@@ -79,7 +85,7 @@ class BoxScoreParser:
 
         #HOME 투수기록
         #col=[u'선수명',u'등판',u'결과',u'승',u'패',u'세',u'이닝',u'타자',u'투구수',u'타수',u'피안타',u'홈런',u'4사구',u'삼진',u'실점',u'자책',u'평균자책점']
-        frame=self.html.select('table#xtable3 tbody:nth-of-type(2) tr')
+        frame=html.select('table#xtable3 tbody:nth-of-type(2) tr')
         data=list()    #frame으로부터 데이터를 담을 리스트
         for row in frame:
             tmp=list()
@@ -92,7 +98,7 @@ class BoxScoreParser:
         
         #AWAY_타자기록
         columns=[u'선수명',u'1',u'2',u'3',u'4',u'5',u'6',u'7',u'8',u'9',u'10',u'11',u'12',u'타수',u'안타',u'타점',u'득점',u'타율']
-        frame=self.html.select('table#xtable1 tbody:nth-of-type(1) tr')
+        frame=html.select('table#xtable1 tbody:nth-of-type(1) tr')
         data=list()
         for row in frame:
             tmp=list()
@@ -103,7 +109,7 @@ class BoxScoreParser:
         
         #HOME_타자기록
         columns=[u'선수명',u'1',u'2',u'3',u'4',u'5',u'6',u'7',u'8',u'9',u'10',u'11',u'12',u'타수',u'안타',u'타점',u'득점',u'타율']
-        frame=self.html.select('table#xtable1 tbody:nth-of-type(2) tr')
+        frame=html.select('table#xtable1 tbody:nth-of-type(2) tr')
         data=list()
         for row in frame:
             tmp=list()
@@ -111,9 +117,4 @@ class BoxScoreParser:
                 tmp.append(dat)
             data.append(tmp)
         self.h_batRecord = pd.DataFrame(data=data,columns=columns)
-
-
-# In[ ]:
-
-
 
