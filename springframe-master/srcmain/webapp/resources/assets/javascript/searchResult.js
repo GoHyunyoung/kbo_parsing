137822aaf970ebdb40f9b0d0cf60c605f2dcd748
gohyunyoung <gohyunyoung98@gmail.com>
2016-08-31 11:51:48 +0900

    ...

--- springframe-master/srcmain/webapp/resources/assets/javascript/searchResult.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by gohyunyoung98 on 16. 7. 25.
- */
-$("input.form-control.form-control-inline.date-picker").on("change", function () {
-    alert("alert");
-});
--- springframe-master/src/main/webapp/resources/assets/javascript/test.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by GoHyunyoung98 on 2016-08-12.
- */
-
-//BEGIN CUSTOMIZING
-$(document).ready(function () {
-    $('div.page-container').fadeIn(2000);
-});
